23989cae447d9a5854003591d8ab3357381905cd
SMMainulHasan <mainulhasan7070@gmail.com>
2021-03-26 01:27:17 +0600

    updated destination

--- src/components/Destination/Destination.js
+++ src/components/Destination/Destination.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router';
 import vehicleData from '../../vehicleData/vehicleData';
 import './Destination.css';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faUserFriends} from '@fortawesome/free-solid-svg-icons';
 
 const Destination = () => {
     const [location, setLocation] = useState({
         from: '',
         to: ''
     });
+
+    const [clickedSearch, setClickedSearch] = useState(false)
     const handleSearchBtn = () => {
-        console.log(location.from, location.to);
+        setClickedSearch(true);
     }
     const {vehicleType} = useParams();
     let vehicleToDisplay ;
@@ -18,27 +23,47 @@ const Destination = () => {
             return vehicleToDisplay = vehicle ;
         }
     })
-    const {image, set, type} = vehicleToDisplay;
+    console.log(clickedSearch);
+    const {image, set, type, rent} = vehicleToDisplay;
     return (
         <div className="destination">
             <div className="location-search">
+            {
+            clickedSearch
+            ? <>
+                    <div className="from-to">
+                        <ul>
+                            <div className="vertical-line"></div>
+                            <li>{location.from}</li>
+                            <br/>
+                            <li>{location.to}</li>
+                        </ul>
+                    </div>
+                    <div className="available-vehicle">
+                        <h5><img src={image} alt=""/> &nbsp; {type}</h5>
+                        <h5><FontAwesomeIcon icon={faUserFriends} /> &nbsp; {set}</h5>
+                        <h4>${rent}</h4>
+                    </div>
+                    <div className="available-vehicle">
+                        <h5><img src={image} alt=""/> &nbsp; {type}</h5>
+                        <h5><FontAwesomeIcon icon={faUserFriends} /> &nbsp; {set}</h5>
+                        <h4>${rent}</h4>
+                    </div>
+                    <div className="available-vehicle">
+                        <h5><img src={image} alt=""/> &nbsp; {type}</h5>
+                        <h5><FontAwesomeIcon icon={faUserFriends} /> &nbsp; {set}</h5>
+                        <h4>${rent}</h4>
+                    </div>
+                </>
+            : <> 
                 <h5>Pick from</h5>
                 <input onBlur={event => setLocation({...location, from: event.target.value})} required name="from" type="text"/>
                 <h5>Pick to</h5>
                 <input onBlur={event => setLocation({...location, to: event.target.value})} required name="to" type="text"/><br/>
                 <button onClick={handleSearchBtn}>Search</button>
-                <div className="from-to">
-                    <ul>
-                        <div className="vertical-line"></div>
-                        <li>{location.from}</li>
-                        <br/>
-                        <li>{location.to}</li>
-                    </ul>
-                </div>
-                <div className="available-vehicle">
-                    <img src={image} alt=""/>
-                    <h5>{set}</h5>
-                </div>
+             </>
+            } 
+                
             </div>
         </div>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Login.css';
+import google from '../../images/google.png';
 
 const Login = () => {
+    //Create user form//
+    const [createUser, setCreatUser] = useState({name:'',email:'',password:'', confirmPassword: ''});
+    const inputHandler = e => {
+        setCreatUser({...createUser, [e.target.name]:e.target.value})
+    }
+
+    //Login user form//
+    const [user, setUser] = useState({email:'',password:''});
+    // const inputHandler = e => {
+    //     setCreatUser({...createUser, [e.target.name]:e.target.value})
+    // }
+    
+
+    //toggle login Or Create form//
+    const [login, setLogin] = useState(true);
+    const toggleLogin =() => setLogin(!login)
     return (
-        <div>
-            <h1>login</h1>
+        <div className="login">
+            {
+                login
+                ?  <from className="create-account">
+                        <h3>Login</h3>
+                        <br/>
+                        <input onBlur={inputHandler} className="input" name="email" required type="text" placeholder="Email"/><br/>
+                        <input onBlur={inputHandler} className="input" name="password" required type="text" placeholder="Password"/><br/>
+                        <input className="button" required type="submit" value="Create an account"/><br/><br/>
+                        <p> Don't have an account?  <span onClick={toggleLogin} className="log-in-link">Create an account</span></p>
+                </from>
+                : <from className="create-account">
+                    <h3>Create an account</h3>
+                    <br/>
+                    <input onBlur={inputHandler} className="input" name="name" required type="text" placeholder="Name"/><br/>
+                    <input onBlur={inputHandler} className="input" name="email" required type="text" placeholder="Email"/><br/>
+                    <input onBlur={inputHandler} className="input" name="password" required type="text" placeholder="Password"/><br/>
+                    <input onBlur={inputHandler} className="input" name="confirmPassword" required type="text" placeholder="Confirm password"/><br/>
+                    <input className="button" required type="submit" value="Create an account"/><br/><br/>
+                    <p> Already have an account?  <span onClick={toggleLogin} className="log-in-link">Login</span></p>
+                </from>
+            }
+            <p>Or</p><br/>
+            <p className="google-btn"> <img src={google} alt=""/> &nbsp; &nbsp; Continue with Google</p>
         </div>
     );
 };
